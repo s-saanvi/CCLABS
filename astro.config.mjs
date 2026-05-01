d56cce2b6d441a8dc19d1ee96a049dd7d67577fc
Saanvi S <mrnags697@gmail.com>
2026-05-01 18:59:12 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://s-saanvi.github.io',
-  base: '/CCLABS',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
