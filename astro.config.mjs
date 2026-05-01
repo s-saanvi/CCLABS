9cf956e0b96ba0c3964bcacedf9838b908b74d74
Saanvi S <mrnags697@gmail.com>
2026-05-01 19:50:28 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://s-saanvi.github.io',
-  base: '/CCLABS',
-  output: 'static',
+  base: '/CCLABS/',
+  //output: 'static',
   integrations: [mdx(), sitemap()],
 });
